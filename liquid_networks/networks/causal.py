fix causal conv + test with batchnorm

liquid_networks/networks/causal.py
@@ -6,19 +6,21 @@ from torch.nn import functional as F
 
 class CausalConv1d(nn.Conv1d):
     def __init__(self, in_channels: int, out_channels: int) -> None:
-        self.__kernel_size = 3
+        dilation = 1
+        kernel_size = 3
+        stride = 2
+
         super().__init__(
             in_channels,
             out_channels,
-            (self.__kernel_size,),
+            (kernel_size,),
             padding=(0,),
-            stride=(2,),
+            stride=(stride,),
+            dilation=dilation,
         )
 
-        self.__padding = self.__kernel_size // 2 + self.__kernel_size % 2
+        self.__padding = (kernel_size - 1) * dilation
 
     # pylint: disable=arguments-renamed
     def forward(self, x: th.Tensor) -> th.Tensor:
-        return super().forward(F.pad(x, (self.__padding, 0)))[
-            :, :, : -self.__padding
-        ]
+        return super().forward(F.pad(x, (self.__padding, 0)))

liquid_networks/networks/cell.py
@@ -12,17 +12,17 @@ class CellModel(nn.Module):
     ) -> None:
         super().__init__()
 
+        std = 1e-3
+
         self.__weights = nn.Linear(input_size, neuron_number, bias=False)
         self.__recurrent_weights = nn.Linear(
             neuron_number, neuron_number, bias=False
         )
-        self.__biases = nn.Parameter(th.randn(1, neuron_number) * 1e-3)
+        self.__biases = nn.Parameter(th.randn(1, neuron_number) * std)
 
         def __init_weights(module: nn.Module) -> None:
             if isinstance(module, nn.Linear):
-                nn.init.xavier_uniform_(module.weight, gain=1e-3)
-                if module.bias is not None:
-                    nn.init.normal_(module.bias, std=1e-3)
+                nn.init.xavier_uniform_(module.weight, gain=std)
 
         self.apply(__init_weights)
 
@@ -41,8 +41,10 @@ class LiquidCell(nn.Module):
     ) -> None:
         super().__init__()
 
-        self.__a = nn.Parameter(th.randn(1, neuron_number))
-        self.__tau = nn.Parameter(th.abs(th.randn(1, neuron_number)))
+        std = 1e-3
+
+        self.__a = nn.Parameter(th.randn(1, neuron_number) * std)
+        self.__tau = nn.Parameter(th.abs(th.randn(1, neuron_number)) * std)
 
         self.__f = CellModel(neuron_number, input_size)
 